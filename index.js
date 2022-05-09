14733c9b25d5cb2a1f39b4045c9cd52f79ef223b
arifa-akter <arifaakterchoity@gmail.com>
2022-05-09 10:33:58 +0600

    add file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
-const port = process.env.port||5000;
+const port = process.env.PORT||5000;
 require('dotenv').config()
 const app = express()
 // middleware
